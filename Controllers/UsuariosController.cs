6308649ff6466f711509f1cffb9d47629dc65b93
LenharesY2K <lenharespedro@gmail.com>
2025-05-12 17:47:39 -0300

    Commit

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using PW3.Entidades;
+using System.Collections.Generic;
 
 namespace Projeto.Controllers
 {
-    public class UsuariosController : Controller
+    public class 
+        : Controller
     {
+        private readonly string connectionString = "Server=localhost;Database=auladb2;Uid=root;Pwd=;";
         public IActionResult Index()
         {
-            return View();
+            List<UsuarioEntidade> model = new List<UsuarioEntidade>();
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new MySqlCommand("SELECT id, email FROM usuarios", connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                UsuarioEntidade u = new UsuarioEntidade();
+                u.Id = reader.GetInt32("id");
+                u.Email = reader.GetString("email");
+                model.Add(u);
+            }
+            connection.Close();
+            return View(model);
         }
     }
 }
